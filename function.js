c35c28916d4e006b68be192d59493ae1445ece4a
Lishaanthi <lishaanthig@gmail.com>
2025-12-20 23:01:38 +0530

    Add functions to log favorite items

---
+++ function.js
@@ -0,0 +1,23 @@
+//prg1
+
+function mad()
+{
+  console.log("Function is working")
+}
+mad()
+
+//prg2
+
+var icecream="vennila"
+var juice="watermelon"
+var food="dosa"
+function favourite()
+{
+  console.log("Favourite icecream:"+icecream)
+  console.log("Favourite juice:"+juice)
+  console.log("Favourite food:"+food)
+}
+favourite()
+
+
+
