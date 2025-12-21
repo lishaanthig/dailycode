1d9664f4f1092a482363954678b020e9819bc4eb
Lishaanthi <lishaanthig@gmail.com>
2025-12-21 14:25:46 +0530

    Add HTML and JavaScript to change title text

---
+++ dom1.js
@@ -0,0 +1,14 @@
+<h1 id="title">Hello World</h1>
+<botton onclick="change()">change</botton>
+<script>
+    var head= document.getElementById("title")
+    console.log(head.textContent)
+    function change()
+    {
+
+    head.textContent="Bye World"
+    }
+
+    
+
+</script>
