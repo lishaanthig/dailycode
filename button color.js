bddc386d174bc94c72a88f8653f4252597409bc3
Lishaanthi <lishaanthig@gmail.com>
2025-12-22 19:52:42 +0530

    Add button to change color on click

---
+++ button color.js
@@ -0,0 +1,8 @@
+<button id="btn" onclick="update()">change color</button>
+<script>
+    var Btn=document.getElementById("btn")
+    function update()
+    {
+        Btn.style.backgroundColor="blue"
+    }
+</script>
