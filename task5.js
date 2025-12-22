1e5596bbff21b5d2377253f8eef1f439f72ede29
Lishaanthi <lishaanthig@gmail.com>
2025-12-22 21:51:23 +0530

    Clean up h1 element variable declarations
    Removed redundant variable declarations for h1 elements.

--- task5.js
+++ task5.js
@@ -5,9 +5,6 @@
 <h1  onclick="deleteitem(event)">Five</h1>
 <h1  onclick="deleteitem(event)">Six</h1>
 <script>
-    var h1=document.getElementById("one")
-    var h1=document.getElementById("two")
-    var h1=document.getElementById("three")
     
     
     function deleteitem(event)
