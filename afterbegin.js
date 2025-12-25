db1e6fc862b977d25663f523b189cd219383f22c
Lishaanthi <lishaanthig@gmail.com>
2025-12-25 15:28:54 +0530

    Create afterbegin.js

---
+++ afterbegin.js
@@ -0,0 +1,12 @@
+<div>
+    <h1>Lishaanthi</h1>
+    <h1>Nishaan</h1>
+    <h1>Sariga</h1>
+    <h1>Sundar</h1>
+    <script>
+        var div=document.querySelector("div")
+        var h1=document.createElement("h1")
+        h1.textContent="List:"
+        div.insertAdjacentElement("afterbegin",h1)
+    </script>
+</div>
