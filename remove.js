2e9a9ca19f9961f564f87e669093df41aa9ef1a0
Lishaanthi <lishaanthig@gmail.com>
2025-12-25 15:38:53 +0530

    we used remove to delete element

---
+++ remove.js
@@ -0,0 +1,11 @@
+<div >
+    <h1 id="one">Lishaanthi</h1>
+    <p id ="two">she is a good skilled girl.She looks gorgeous</p>
+</div>
+<script>
+    var h1=document.getElementById("one")
+    var para=document.getElementById("two")
+    h1.remove()
+    
+    
+</script>
